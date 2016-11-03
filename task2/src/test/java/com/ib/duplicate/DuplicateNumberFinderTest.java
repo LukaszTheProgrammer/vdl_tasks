c22fa6b21824c86048ea8f6b67868bd6f42b5d73
Lukasz Odulinski <lukasz.odulinski.ctr@sabre.com>
2016-11-03 17:55:10 +0100

    reducing test phone book size

--- task2/src/test/java/com/ib/duplicate/DuplicateNumberFinderTest.java
+++ task2/src/test/java/com/ib/duplicate/DuplicateNumberFinderTest.java
@@ -7,25 +7,27 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 
+import static java.lang.System.getProperty;
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by lukasz.odulinski on 30.10.2016.
  */
 public class DuplicateNumberFinderTest {
-
-    public static final String INPUT = System.getProperty("user.dir") + "/src/main/resources/phone_numbers.txt";
+    private static final String SEP = File.separator;
+    private static final String TMP_DIRECTORY = getProperty("user.dir")+ SEP;
+    private static final String INPUT = TMP_DIRECTORY + "phone_numbers.txt";
 
     @Before
     public void setup() {
         if (!(new File(INPUT).exists())) {
-            PhoneBookGenerator.generate(INPUT, 100_000_000, 6745132);
+            PhoneBookGenerator.generate(INPUT, 1_000_000, 6745132);
         }
     }
 
     @Test
     public void shouldFindDuplicateNumber() throws IOException {
         DuplicateNumberFinder duplicateNumberFinder = new DuplicateNumberFinder();
-        assertEquals("0019503830", duplicateNumberFinder.find(INPUT).get());
+        assertEquals("0069538185", duplicateNumberFinder.find(INPUT).orElse(""));
     }
 }
